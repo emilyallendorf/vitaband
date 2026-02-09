b53da48fe4310b6722ed49c48c46d8ab6f2ebcad
Vidhi Challani <vchallan@andrew.cmu.edu>
2026-02-09 11:24:00 -0500

    Initial Commit

---
+++ VitaBand/VitaBandApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaBandApp.swift
+//  VitaBand
+//
+//  Created by Vidhi Challani on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct VitaBandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
